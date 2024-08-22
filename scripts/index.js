235d8948eae727ad4688476767d0c8e256b5c7d3
wolfe1911 <162520914+wolfe1911@users.noreply.github.com>
2024-08-21 23:32:33 -0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -25,19 +25,59 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
-
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const closeModalButton = profileEditModal.querySelector(
   "#profile-edit-modal-close"
 );
 
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+const profileNameInput = document.querySelector("input[name='title']");
+const profileDescriptionInput = document.querySelector(
+  "input[name='description']"
+);
+
+const saveButton = document.querySelector(".modal__button");
+
 profileEditButton.addEventListener("click", () => {
+  profileNameInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+
   profileEditModal.classList.add("modal__opened");
 });
 
 closeModalButton.addEventListener("click", () => {
   profileEditModal.classList.remove("modal__opened");
+});
+
+saveButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  profileTitle.textContent = profileNameInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+
+  profileEditModal.classList.remove("modal__opened");
+});
+
+function getCardElement(data) {
+  const template = document.querySelector("#card-template").content;
+  const cardElement = template.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__text");
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+
+  return cardElement;
+}
+
+const cardList = document.querySelector(".cards__list");
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardList.append(cardElement);
 });
 
